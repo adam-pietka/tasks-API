fd0824e67253bbcefd0b0a69bba3493d3aafc4b6
Adam Pietka <adam.pietk@gmail.com>
2021-05-03 16:53:41 +0200

    Twój controller powinien wyglądać następująco:

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -2,10 +2,10 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
 @Getter
 @AllArgsConstructor
 public class TaskDto {
+
     private Long id;
     private String title;
     private String content;
