d54fe2ee4e26dd622a0bf2990a6c2f302587ad2d
Adam Pietka <adam.pietk@gmail.com>
2021-05-17 22:00:15 +0200

    22.2. Rest Template

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/trello")
@@ -17,7 +20,7 @@ public class TrelloController {
     private final TrelloClient trelloClient;
 
     @GetMapping("getTrelloBoards")
-    public void getTrelloBoards() { // zamianst void ma być List<TrelloBoardDto>
+    public List<TrelloBoardDto> getTrelloBoards() { // zamianst void ma być List<TrelloBoardDto>
 
         // GET request
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
@@ -33,18 +36,8 @@ public class TrelloController {
                 );
             });
         });
-/*
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-        trelloBoards.stream()
-                .filter(trelloBoardDto -> trelloBoardDto.getName().toUpperCase().contains("KODILLA"))
-                .filter(trelloBoardDto -> (trelloBoardDto.getId().length()) > 0 )
-                .forEach(e->{
-                    System.out.println(e.getId() + " - " + e.getName());
-                });
-*/
 
-        /*trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-        });*/
+        return Optional.of(trelloBoards)
+                .orElse(Collections.emptyList());
     }
 }
